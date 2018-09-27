a492f75f9406615e8e7ce9d10265da47022dd680
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-09-27 16:01:52 +0200

    add configs for VBF Higgs and ttbar

--- TTbar_cfg.py
+++ TTbar_cfg.py
@@ -14,8 +14,11 @@ process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 
 # Define files of dataset
 files = FileUtils.loadListFromFile(
-    "data/CMS_MonteCarlo2012_Summer12_DR53X_GluGluToHToTauTau_M-125_8TeV-powheg-pythia6-tauPolarOff_AODSIM_PU_S10_START53_V19-v1_00000_file_index.txt"
+    "data/CMS_MonteCarlo2012_Summer12_DR53X_TTbar_8TeV-Madspin_aMCatNLO-herwig_AODSIM_PU_S10_START53_V19-v2_00000_file_index.txt"
 )
+files.extend(FileUtils.loadListFromFile(
+    "data/CMS_MonteCarlo2012_Summer12_DR53X_TTbar_8TeV-Madspin_aMCatNLO-herwig_AODSIM_PU_S10_START53_V19-v2_20000_file_index.txt"
+))
 process.source = cms.Source(
     "PoolSource", fileNames=cms.untracked.vstring(*files))
 
@@ -25,6 +28,6 @@ process.source.skipEvents = cms.untracked.uint32(0)
 # Register fileservice for output file
 process.aod2nanoaod = cms.EDAnalyzer("AOD2NanoAOD")
 process.TFileService = cms.Service(
-    "TFileService", fileName=cms.string("GluGluToHToTauTau.root"))
+    "TFileService", fileName=cms.string("TTbar.root"))
 
 process.p = cms.Path(process.aod2nanoaod)
