4866373f6edb06af0053d5dcb146f6a2ec263e54
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-10-04 08:35:01 +0200

    add script to merge files of jobs

---
+++ merge_jobs.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import sys
+import re
+
+
+def parse_arguments():
+    if not len(sys.argv) == 2:
+        raise Exception("Run with './merge_jobs.py path/to/input/directory'.")
+    return sys.argv[1]
+
+
+def main(input_dir):
+    # Sanitize input dir string
+    if input_dir[-1] == "/":
+        input_dir = input_dir[:-1]
+
+    # Sanitize input directory
+    print("Input directory: %s"%(input_dir))
+    if not os.path.exists(input_dir):
+        raise Exception("Input directory does not exist: %s"%(input_dir))
+    if not os.path.isdir(input_dir):
+        raise Exception("Input is no directory: %s"%(input_dir))
+
+    # Extract process from path
+    process = os.path.basename(input_dir)
+    print("Process: %s"%(process))
+
+    # Get expected number of files
+    if not os.path.exists("data/") or not os.path.isdir("data/"):
+        raise Exception("Directory \"data\" does not exist.")
+    count_expected = 0
+    for f in os.listdir("data/"):
+        if process in f:
+            count_expected += len(open(os.path.join("data", f)).readlines())
+    print("Expect %u files in input directory."%(count_expected))
+
+    # Go through files and find missing ones
+    files = {}
+    for f in os.listdir(input_dir):
+        if not process+"_" in f:
+            raise Exception("File %s does not match job file."%(f))
+        n = re.search("%s_(.*).root"%(process), f).group(1)
+        files[int(n)] = os.path.join(input_dir, f)
+
+    missing_file = False
+    for i in range(count_expected):
+        if not i in files:
+            print("Miss file with ID %u."%(i))
+            missing_file = True
+    print("Found %u files in input directory."%(len(files)))
+    if missing_file:
+        raise Exception("Found missing files.")
+
+    # Merge files
+    chain = ROOT.TChain("aod2nanoaod/Events")
+    for f in files.values():
+        chain.Add(f)
+    output_path = os.path.join(input_dir.replace(process, ""), process+".root")
+    chain.Merge(output_path)
+    print("Wrote merged file to %s."%(output_path))
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
