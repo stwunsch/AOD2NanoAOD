b21e1f19892e1bf23f8699933c7babb09b2da09e
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-03-08 13:29:41 +0100

    implement readout of trigger paths

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Define path for job directories
-BASE_PATH=/ceph/wunsch/cms_opendata_2012_jobs_2019-03-07
+BASE_PATH=/ceph/wunsch/cms_opendata_2012_jobs_2019-03-08
 mkdir -p $BASE_PATH
 
 # Set processes
 PROCESSES=( \
     #SMHiggsToZZTo4L \
-    #GluGluToHToTauTau \
-    #VBF_HToTauTau \
     #ZZTo2e2mu \
     #ZZTo4mu \
     #ZZTo4e \
+    GluGluToHToTauTau \
+    VBF_HToTauTau \
     TTbar \
     W1JetsToLNu \
     W2JetsToLNu \
@@ -22,6 +22,8 @@ PROCESSES=( \
     DY4JetsToLL \
     Run2012B_DoubleMuParked \
     Run2012C_DoubleMuParked \
+    Run2012B_SingleMuParked \
+    Run2012C_SingleMuParked \
     Run2012B_DoubleElectron \
     Run2012C_DoubleElectron \
     )
