7449d76b14f98f55028a4e900595f12bba29fafd
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-05-06 13:51:48 +0200

    fix genmatching for taus

--- src/AOD2NanoAOD.cc
+++ src/AOD2NanoAOD.cc
@@ -68,25 +68,34 @@ const static std::vector<std::string> interestingTriggers = {
 };
 
 template <typename T>
-void subtractInvisible(reco::Candidate::LorentzVector &p4, T &x) {
-  for (auto d = x->begin(); d != x->end(); d++) {
-    const auto pdgId = d->pdgId();
-    if (std::abs(pdgId) == 12 || std::abs(pdgId) == 14 ||
-        std::abs(pdgId) == 16 || std::abs(pdgId) == 18) {
-      p4 = p4 - d->p4();
-    }
-    subtractInvisible(p4, d);
-  }
-}
-
-template <typename T>
-int findBestMatch(T& gens, reco::Candidate::LorentzVector& p4, bool subtract_invisible = false) {
+int findBestVisibleMatch(T& gens, reco::Candidate::LorentzVector& p4) {
   float minDeltaR = 999.0;
   int idx = -1;
   for (auto g = gens.begin(); g != gens.end(); g++) {
     auto tmp_p4 = g->p4();
-    if (subtract_invisible) subtractInvisible(tmp_p4, g);
+    auto daughters = g->daughterRefVector();
+    for (auto d = daughters.begin(); d != daughters.end(); d++) {
+      const auto pdgId = (*d)->pdgId();
+      if (std::abs(pdgId) == 12 || std::abs(pdgId) == 14 ||
+          std::abs(pdgId) == 16 || std::abs(pdgId) == 18) {
+        tmp_p4 = tmp_p4 - (*d)->p4();
+      }
+    }
     const auto tmp = deltaR(tmp_p4, p4);
+    if (tmp < minDeltaR) {
+      minDeltaR = tmp;
+      idx = g - gens.begin();
+    }
+  }
+  return idx;
+}
+
+template <typename T>
+int findBestMatch(T& gens, reco::Candidate::LorentzVector& p4) {
+  float minDeltaR = 999.0;
+  int idx = -1;
+  for (auto g = gens.begin(); g != gens.end(); g++) {
+    const auto tmp = deltaR(g->p4(), p4);
     if (tmp < minDeltaR) {
       minDeltaR = tmp;
       idx = g - gens.begin();
@@ -707,7 +716,7 @@ void AOD2NanoAOD::analyze(const edm::Event &iEvent,
     for (auto p = selectedTaus.begin(); p != selectedTaus.end(); p++) {
       // Gen particle matching
       auto p4 = p->p4();
-      auto idx = findBestMatch(interestingGenParticles, p4, true); // TODO: Subtract the invisible parts only once.
+      auto idx = findBestVisibleMatch(interestingGenParticles, p4); // TODO: Subtract the invisible parts only once.
       if (idx != -1) {
         auto g = interestingGenParticles.begin() + idx;
         value_gen_pt[value_gen_n] = g->pt();
